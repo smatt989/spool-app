b2cd8ed7ac9aebd536d8470725c98565c43ef5ce
Andrew Ckor <megakor@gmail.com>
2017-01-26 01:06:03 +0200

    Massive new looks and custom views

---
+++ Spool/AdventuresNavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  AdventuresNavigationController.swift
+//  Spool
+//
+//  Created by Andrew Ckor on 25/01/2017.
+//  Copyright © 2017 Matthew Slotkin. All rights reserved.
+//
+
+import UIKit
+
+class AdventuresNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        // Style the navbar
+        self.navigationBar.tintColor = UIColor(red:0.40, green:0.45, blue:0.90, alpha:1.0) //#6772e5
+        self.navigationBar.titleTextAttributes = [
+            NSFontAttributeName : UIFont(name: "Nunito-Bold", size: 19)! ,
+            NSForegroundColorAttributeName : UIColor(red:0.20, green:0.20, blue:0.36, alpha:1.0) ] //#32325d
+    }
+
+}
