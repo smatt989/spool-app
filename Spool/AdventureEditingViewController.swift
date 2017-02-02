19b4091d20900b3565e118a6a36463347f73af14
Matthew Slotkin <matt@Matthews-MacBook-Pro.local>
2017-02-01 16:10:15 -0800

    Revert "making funny progress on controller:"
    This reverts commit 6dfd2ca2c8489db87e2cc413c8dc265f52f8524e.

--- Spool/AdventureEditingViewController.swift
+++ Spool/AdventureEditingViewController.swift
@@ -81,6 +81,12 @@ class AdventureEditingViewController: UIViewController, MKMapViewDelegate, UIGes
         press.delegate = self
         
         mapView.addGestureRecognizer(press)
+        
+        waypointToolbar.isHidden = true
+        waypointToolbar.mapview = mapView
+        waypointToolbar.removeMarkerCallback = removeAnnotation
+        waypointToolbar.addRadiusCircle = addRadiusCircle
+        waypointToolbar.removeCircle = removeCircle
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -243,6 +249,7 @@ class AdventureEditingViewController: UIViewController, MKMapViewDelegate, UIGes
     }
     
     func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        dismissToolbar()
         view.isSelected = true }
     
     // Renders the map route
@@ -324,6 +331,68 @@ class AdventureEditingViewController: UIViewController, MKMapViewDelegate, UIGes
     }
     
     override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+    }
+    
+    
+    private func dismissToolbar() {
+        waypointToolbar.dismissSelf()
+    }
+    
+    @IBOutlet weak var waypointToolbar: WaypointToolbar!
+    
+    var pinchGestureRecognizer: UIPinchGestureRecognizer?
+    
+    private func addPinchGesture() {
+        pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinchedView(_:)))
+        pinchGestureRecognizer!.delegate = self
+        mapView.isZoomEnabled = false
+        //mapView.isUserInteractionEnabled = false
+        mapView.addGestureRecognizer(pinchGestureRecognizer!)
+    }
+    
+    private func removePinchGesture() {
+        mapView.isZoomEnabled = true
+        //mapView.isUserInteractionEnabled = true
+        mapView.removeGestureRecognizer(pinchGestureRecognizer!)
+    }
+    
+    private var circle: MKCircle? {
+        didSet {
+            if circle != nil {
+                waypointToolbar.updateRange(radius: circle!.radius)
+            }
+        }
+    }
+    
+    func addRadiusCircle(location: CLLocation, radius: CLLocationDistance){
+        print("ADDING THIS RADIUS: \(radius)")
+        circle = MKCircle(center: location.coordinate, radius: radius as CLLocationDistance)
+        mapView.add(circle!)
+        addPinchGesture()
+    }
+    
+    @objc
+    func pinchedView(_ sender:UIPinchGestureRecognizer){
+        print("PINCHING...")
+        updateCircle(radius: (circle!.radius * Double(sender.scale)) as CLLocationDistance)
+        sender.scale = 1.0
+    }
+    
+    private func updateCircle(radius: CLLocationDistance) {
+        print("radius \(radius)")
+        let newCircle = MKCircle(center: circle!.coordinate, radius: radius)
+        DispatchQueue.main.async { [weak weakself = self] in
+            weakself!.mapView.remove(weakself!.circle!)
+            weakself!.mapView.add(newCircle)
+            weakself!.circle = newCircle
+        }
+    }
+    
+    func removeCircle() {
+        if circle != nil {
+            removePinchGesture()
+            mapView!.remove(circle!)
+        }
     }
 }
 
