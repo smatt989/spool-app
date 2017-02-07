da040b43b2e4d225f6a2687e6c42334bffaf3e7b
Andrew Ckor <megakor@gmail.com>
2017-02-08 00:39:53 +0200

    New restart/continue buttons

--- Spool/RoundedUIButton.swift
+++ Spool/RoundedUIButton.swift
@@ -17,6 +17,18 @@ class RoundedUIButton: UIButton {
         }
     }
     
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
